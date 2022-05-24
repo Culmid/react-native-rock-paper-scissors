7f1da238da7a3cf372ae9fd08f56c32d29f4b531
Cullen Middleton <culmid@gmail.com>
2022-05-24 16:49:45 +0200

    Create Game View

---
+++ views/GameView/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./style";
+import { View, Text } from "react-native";
+
+function GameView() {
+  return (
+    <View>
+      <Text>Rock Paper Scissors</Text>
+    </View>
+  );
+}
+
+export default GameView;
